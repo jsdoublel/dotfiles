70cc2c0ff3bf509d67dc809cfcf14a3a7ff31508
James Willson <jsdoublel@gmail.com>
2024-02-27 13:36:23 -0600

    small audio_toggle fix

--- i3/scripts/audio_toggle.sh
+++ i3/scripts/audio_toggle.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 current="$(pactl info | grep Sink: | cut -d " " -f 3)"
-if [ "$current" = "alsa_output.pci-0000_05_00.0.5.analog-stereo" ]; then 
+if [ "1" = "$(pactl info | grep Sink: | grep Logitech | wc -l)" ]; then 
+    echo "$(pactl list sinks short | grep "alsa_output.pci-0000_05" | cut -d $'\t' -f 1)"
+    pactl set-default-sink "$(pactl list sinks short | grep "alsa_output.pci-0000_05" | cut -d $'\t' -f 1)"
+    echo "Switched to speakers"
+else
+    echo "$(pactl list sinks short | grep "Logitech" | cut -d $'\t' -f 1)"
     pactl set-default-sink "$(pactl list sinks short | grep "Logitech" | cut -d $'\t' -f 1)"
     echo "Switched to headphones"
-else
-    pactl set-default-sink "$(pactl list sinks short | grep "alsa_output.pci-0000_05_00.0.5.analog-stereo" | cut -d $'\t' -f 1)"
-    echo "Switched to speakers"
 fi
 
