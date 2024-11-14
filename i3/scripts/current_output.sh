b987ac42b0045cabca3b2371f4e77075d4e95449
James Willson <jsdoublel@gmail.com>
2024-11-14 01:44:21 -0600

    changed i3 bar a bit

--- i3/scripts/current_output.sh
+++ i3/scripts/current_output.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 if [ "1" = "$(pactl info | grep Sink | grep Logitech | wc -l)" ]; then 
-    echo ": Headphones"
+    echo "Headphones"
 else
-    echo ": Speakers  "
+    echo " Speakers "
 fi
 
 # print red on mute
 if [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ]; then
 	echo ""
-	echo '#BF616A'
+	echo '#FF0000'
+	# echo '#BF616A'
+else
+	echo ""
+	# echo '#00FF00'
 fi
