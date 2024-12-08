a584982ccedcac0caa62451640b296752af9741e
James Willson <jsdoublel@gmail.com>
2024-12-07 18:40:14 -0600

    fixed notifications

--- i3/scripts/volume_brightness.sh
+++ i3/scripts/volume_brightness.sh
@@ -29,11 +29,11 @@ function get_volume_icon {
     volume=$(get_volume)
     mute=$(get_mute)
     if [ "$volume" -eq 0 ] || [ "$mute" == "yes" ] ; then
-        volume_icon=""
+        volume_icon="   "
     elif [ "$volume" -lt 50 ]; then
-        volume_icon=""
+        volume_icon="   "
     else
-        volume_icon=""
+        volume_icon="   "
     fi
 }
 
@@ -46,7 +46,8 @@ function get_brightness_icon {
 function show_volume_notif {
     volume=$(get_mute)
     get_volume_icon
-    dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
+    # dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
+    dunstify -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
 }
 
 # Displays a brightness notification using dunstify
