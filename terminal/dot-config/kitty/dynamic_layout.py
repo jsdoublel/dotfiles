89e614eec5bdcbe079d334f6d29c5bd4b7440e5b
James Willson <jamessw156@gmail.com>
2026-05-14 00:12:12 -0500

    improvements to dynamic_layout.py

--- terminal/dot-config/kitty/dynamic_layout.py
+++ terminal/dot-config/kitty/dynamic_layout.py
@@ -3,20 +3,19 @@ from kitty.boss import Boss
 from kitty.window import Window
 from kitty.fast_data_types import viewport_for_window
 
+TOTAL_LAYOUTS = 7  # Number of total layouts
+
 
 def layout_name(short_layout: str) -> str:
-    return f"{short_layout}:bias=50;full_size=1;mirrored=false"
+    return f"{short_layout}:bias=55;full_size=1;mirrored=false"
 
 
 def on_resize(boss: Boss, window: Window, data: Dict[str, Any]) -> None:
-    if boss.active_tab is None:
-        return
-    try:
-        _, _, width, height, _, _ = viewport_for_window(window.os_window_id)
-    except Exception:
-        return
+    assert boss.active_tab is not None, "No active tab"
+    _, _, width, height, _, _ = viewport_for_window(window.os_window_id)
     new_layout = layout_name("tall") if width >= height else layout_name("fat")
-    if new_layout not in boss.active_tab.enabled_layouts:
+    cur_layouts = boss.active_tab.enabled_layouts
+    if new_layout not in cur_layouts or len(cur_layouts) == TOTAL_LAYOUTS:
         cur_layout = boss.active_tab.current_layout.name
         boss.active_tab.set_enabled_layouts([new_layout, "stack"])
         if cur_layout != "stack":
