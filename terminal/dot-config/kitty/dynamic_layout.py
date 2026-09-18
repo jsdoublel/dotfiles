df6ca5ad53f88385d97378e1b143927ef59b2bb7
James Willson <jamessw156@gmail.com>
2026-09-18 00:59:36 -0500

    update to kitty config

--- terminal/dot-config/kitty/dynamic_layout.py
+++ terminal/dot-config/kitty/dynamic_layout.py
@@ -11,16 +11,17 @@ def layout_name(short_layout: str) -> str:
 
 
 def on_resize(boss: Boss, window: Window, data: Dict[str, Any]) -> None:
-    if boss.active_tab is None:
+    tab = window.tabref()
+    if tab is None:
         return
     try:
         _, _, width, height, _, _ = viewport_for_window(window.os_window_id)
     except:
         return
     new_layout = layout_name("tall") if width >= height else layout_name("fat")
-    cur_layouts = boss.active_tab.enabled_layouts
+    cur_layouts = tab.enabled_layouts
     if new_layout not in cur_layouts or len(cur_layouts) == TOTAL_LAYOUTS:
-        cur_layout = boss.active_tab.current_layout.name
-        boss.active_tab.set_enabled_layouts([new_layout, "stack"])
+        cur_layout = tab.current_layout.name
+        tab.set_enabled_layouts([new_layout, "stack"])
         if cur_layout != "stack":
-            boss.active_tab.goto_layout(new_layout)
+            tab.goto_layout(new_layout)
